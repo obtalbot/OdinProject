b3e9ea74b07b32a8ae40f0f4ab3606e08257da43
obtalbot <obtalbot@users.noreply.github.com>
2014-09-22 18:04:40 -0700

    Change "new_message" to "cipher"

--- RubyBuildingBlocks/caesar_cipher.rb
+++ RubyBuildingBlocks/caesar_cipher.rb
@@ -1,18 +1,18 @@
 def caesar_cipher(message, shift_factor)
   alphabet = [*("a".."z")]
-  new_message = ""
+  cipher = ""
 
   message.each_char do |char|
     if /[a-zA-Z]/.match(char)
       new_char = alphabet[alphabet.find_index(char.downcase) + shift_factor - alphabet.length]
       new_char.upcase! if char == char.upcase
-      new_message << new_char
+      cipher << new_char
     else 
-      new_message << char
+      cipher << char
     end
   end
 	
-  new_message
+  cipher
 end	
 
 p caesar_cipher("What a string!", 5)
