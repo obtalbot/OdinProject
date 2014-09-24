79cbb379d48d4de20ece58602f0c49eb05d36c9d
O. Bucao <orlando.bucao@biola.edu>
2014-09-24 10:48:07 -0700

    Refactor tictactoe.rb

--- OOP/tictactoe.rb
+++ OOP/tictactoe.rb
@@ -15,7 +15,7 @@ class Board
 	end
 
 	def set_cell(number, symbol)
-    unless @occupied_cells.include? number
+    if Game::VALID_MOVES.include?(number) && !@occupied_cells.include?(number) && 
       cell = get_cell_mapping(number)
       @grid[cell[0]][cell[1]] = symbol
       @occupied_cells << number
@@ -40,20 +40,57 @@ class Board
 end
 
 
+class Player
+  attr_accessor :name, :symbol, :moves
+
+  def initialize(name=nil, symbol=nil)
+    @name = name
+    @symbol = symbol
+    @moves = []
+  end
+
+  def to_s
+    @name
+  end
+
+  def make_move(board)
+    valid_move_made = false
+    until valid_move_made do
+      print "#{self.name}'s move => "
+      move = gets.chomp.to_i
+
+      if board.set_cell(move,@symbol)
+        @moves << move
+        valid_move_made = true
+      else
+        return false
+      end
+    end
+    valid_move_made
+  end
+
+  def win?
+    win = false
+    Game::WINNING_MOVES.each do |combo|
+      if combo.to_set.subset? @moves.to_set then
+        win = true
+      end
+    end
+    win
+  end
+end
+
+
 class Game
   require 'set'
 
-  VALID_SYMBOLS = ["X","O"]
+  VALID_MOVES = [*(1..9)]
   WINNING_MOVES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
   def initialize
-    @player1 = Player.new
-    @player2 = Player.new
-    setup(@player1)
-    setup(@player2)
-    @winner_found = false
+    @player1 = Player.new("Bob","x")
+    @player2 = Player.new("Frank","o")
     @board = Board.new
-    return nil
   end
 
 
@@ -62,54 +99,26 @@ class Game
 
     current_player = @player1
     next_player = @player2
+    winner_found = false
 
-    while !@winner_found do
-      get_move(current_player)
+    9.times do
+      until current_player.make_move(@board) do
+        puts "Invalid move. Try again."
+      end
+
       @board.display_board
-      check_for_win(current_player)
 
-      if @winner_found then
+      if current_player.win? then
         puts "#{current_player} Wins!"
+        return
       end
 
       next_player, current_player = current_player, next_player
     end
-  end
 
-  def setup(player)
-    print "Player name: "
-    player.name = gets.chomp
-    print "Player symbol: "
-    player.symbol = gets.chomp
-  end
-
-  def get_move(player)
-    print "Your move #{player.name} => "
-    move = gets.chomp.to_i
-    player.moves << move
-    @board.set_cell(move,player.symbol)
-  end
-
-  def check_for_win(player)
-    WINNING_MOVES.each do |winning_combo|
-      if winning_combo.to_set.subset? player.moves.to_set then
-        @winner_found = true
-        break
-      end
-    end
+    puts "No winner."
   end
 end
 
-class Player
-  attr_accessor :name, :symbol, :moves
-
-  def initialize
-    @name = ""
-    @symbol = ""
-    @moves = []
-  end
-
-  def to_s
-    @name
-  end
-end
+game = Game.new
+game.play
